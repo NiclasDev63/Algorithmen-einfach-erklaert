a3e76655db4fa564941d4d5281874b39c064aac3
NiclasDev63 <niclas.gregor20@gmail.com>
2022-01-29 13:33:14 +0100

    .

--- BubbleSort/BubbleSort.py
+++ BubbleSort/BubbleSort.py
@@ -3,21 +3,17 @@ def bubbleSort(list):
     # n entspricht der Länge der Liste
     n = len(list)
 
-    """
-    Hier beginnt die erste Schleife.
-    Die schleife start bei 0 und geht bis n-1.
-    Nach jedem durchlauf erhöhrt sich i um den Wert 1, also
-    Durchlauf 1: i = 0
-    Durchlauf 2: i = 1
-    Durchlauf 3: i = 2
-    ...
-    Durchlauf n: i = n-1
-    """
+    # Hier beginnt die erste schleife
+    # Diese startet bei 0 und geht bis n-1
     for i in range(n):
 
-
+        
+        # Hier beginnt die zweite Schleife.
+        # Diese schleife geht jedoch nur bis n-i-1
         for j in range(n-i-1):
 
+            # Hier wird überprüft ob das Element an der Stelle j größer ist, als das Element eins weiter vorne.
+            # Falls dies der Fall ist, werden die Elemente getauscht
             if list[j] > list[j+1]:
                 list[j], list[j+1] = list[j+1], list[j]
 
